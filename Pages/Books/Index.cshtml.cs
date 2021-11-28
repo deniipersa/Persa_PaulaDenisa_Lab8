3876ba03baad09f1645fa1fc2ad86b272a8c166c
Paula-Denisa Perșa <denisa.persa@yahoo.com>
2021-11-28 18:48:16 +0200

    Add project files.

---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Persa_PaulaDenisa_Lab8.Data;
+using Persa_PaulaDenisa_Lab8.Models;
+
+namespace Persa_PaulaDenisa_Lab8.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Persa_PaulaDenisa_Lab8.Data.Persa_PaulaDenisa_Lab8Context _context;
+
+        public IndexModel(Persa_PaulaDenisa_Lab8.Data.Persa_PaulaDenisa_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book
+            .Include(b => b.Publisher)
+            .ToListAsync();
+        }
+    }
+}
---
+++ Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Persa_PaulaDenisa_Lab8.Data;
+using Persa_PaulaDenisa_Lab8.Models;
+
+namespace Persa_PaulaDenisa_Lab8.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Persa_PaulaDenisa_Lab8.Data.Persa_PaulaDenisa_Lab8Context _context;
+
+        public IndexModel(Persa_PaulaDenisa_Lab8.Data.Persa_PaulaDenisa_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Category> Category { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Category = await _context.Category.ToListAsync();
+        }
+    }
+}
